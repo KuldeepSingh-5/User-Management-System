908e8daefc7b4aed696894b86f73fb46ff8f8d9a
KuldeepSingh-5 <kuldeepbaghel51298@gmail.com>
2026-05-05 18:08:02 +0530

    changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 require("./db/conn");
 const router = require("./Routes/router")
-const PORT = 6010;
+const PORT = process.env.PORT || 6010;
 
 app.use(cors());
 app.use(express.json());
